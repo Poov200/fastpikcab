b1c84292ef1e174b526b1300ea22530993f42c12
“bharath <bharathwaj9600@gmail.com>
2025-09-08 16:57:56 +0530

    kerela

--- resources/views/layouts/thiruvarur/thiruvarur-to-kerala.blade.php
+++ resources/views/layouts/thiruvarur/thiruvarur-to-kerala.blade.php
@@ -259,7 +259,8 @@
     @include('layouts.footer')
 
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places&callback=initMap" async defer></script>
-</body>
+  <script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAFS0STKnYFbgLIwD6MH37_Zymm388hUO8&libraries=places&callback=initMap"
+        async defer></script></body>
 
 </html>
