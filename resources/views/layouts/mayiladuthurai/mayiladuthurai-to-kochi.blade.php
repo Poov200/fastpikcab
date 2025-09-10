d704789b2f76f40081dc218880c415f32eaaf0f6
Sanjaay <sanjaay@thikse.in>
2025-09-10 12:59:04 +0530

    changes

--- resources/views/layouts/mayiladuthurai/mayiladuthurai-to-kochi.blade.php
+++ resources/views/layouts/mayiladuthurai/mayiladuthurai-to-kochi.blade.php
@@ -226,18 +226,7 @@
                     </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200">
-                    <tr class="hover:bg-gray-50">
-                        <td class="px-6 py-4 font-medium">Hatchback</td>
-                        <td class="px-6 py-4 text-center">₹6,000</td>
-                        <td class="px-6 py-4 text-center">₹12,000</td>
-                        <td class="px-6 py-4">
-                            <ul class="list-disc list-inside text-sm text-gray-600">
-                                <li>Air-conditioned</li>
-                                <li>Up to 4 passengers</li>
-                                <li>Suitable for light luggage</li>
-                            </ul>
-                        </td>
-                    </tr>
+                    
                     <tr class="hover:bg-gray-50">
                         <td class="px-6 py-4 font-medium">Sedan</td>
                         <td class="px-6 py-4 text-center">₹7,000</td>
