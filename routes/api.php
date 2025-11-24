766c320d92a93183263d8570060392ebbef63b84
Poovarasan S <s.poovarasanpoov@gmail.com>
2025-11-24 18:47:39 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+Route::post('/add/bookings', [BookingController::class, 'store']);
+Route::get('/pricings', [PricingController::class, 'index']);
 
 
 
